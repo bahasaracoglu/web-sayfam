a43ebb49864eb4ed75108625b4addea103670636
bahasaracoglu <bahasaracoglu@gmail.com>
2023-05-11 22:53:02 +0300

    6.şekil profile css

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,17 +1,22 @@
 export default function Profile() {
   return (
-    <section id="Profile" className=" py-[5rem] bg-[#F4F4F4] dark:bg-[#2A262B]">
-      <div className="w-7/12 mx-auto max-w-[1440px] ">
+    <section
+      id="Profile"
+      className=" py-[4.5rem] bg-[#F4F4F4] dark:bg-[#2A262B]"
+    >
+      <div className="w-7/12 mx-auto max-w-[1440px] relative">
+        <div className="absolute w-[10.9%] aspect-square rounded-full  border-[21px] border-solid border-[#EA2678]  dark:bg-[#525252] right-[-18.8%] top-[-25.1%]"></div>
+
         <h1
           className="text-center text-[#000000] dark:text-[#FFFFFF]"
           id="section-title-small"
         >
           Profile
         </h1>
-        <div className="flex flex-row justify-between rounded-[12px] text-[#000000] dark:text-[#FFFFFF] ">
+        <div className="flex flex-row justify-center  rounded-[12px] text-[#000000] dark:text-[#FFFFFF] ">
           <div
             id="Basic-information"
-            className="flex w-[46.5%] max-w-[500px] flex-col  p-10 gap-5 mt-10 bg-[#FFFFFF] dark:bg-[#525252] shadow-gray"
+            className="flex w-[46.5%] max-w-[500px] flex-col ml-7  p-10 gap-5 mt-[2.3rem] bg-[#FFFFFF] dark:bg-[#525252] shadow-gray"
           >
             <h4 id="basic-info" className="text-[#EA2678] ">
               Basic Information
@@ -37,9 +42,9 @@ export default function Profile() {
               </li>
             </ul>
           </div>
-          <div className="flex flex-col justify-center w-6/12 p-10 mt-10 gap-5">
+          <div className="flex flex-col justify-center w-6/12 ml-[1.4rem] p-10 mt-[1.5rem] gap-5">
             <h4 id="about-me-title">About me</h4>
-            <p id="about-me">
+            <p id="about-me" className="mb-1">
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam
               aut, odit laborum aliquam voluptatum nisi mollitia.
             </p>
